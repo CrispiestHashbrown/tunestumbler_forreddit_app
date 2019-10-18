1bcf682ce3da0c57946e7268d7567e012082ad42
CrispiestHashbrown <r_kieu@outlook.com>
2019-10-18 11:49:22 +0100

    Revert axios baseurl and build

--- src/axios-urls/axios-tunestumbler.js
+++ src/axios-urls/axios-tunestumbler.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://tunestumblerapi-env.acddbsmryd.us-west-1.elasticbeanstalk.com/tunestumbler-wrapper-for-reddit'
+    baseURL: 'https://api.tunestumbler.com/tunestumbler-wrapper-for-reddit'
     // baseURL: 'http://localhost:8080/tunestumbler-wrapper-for-reddit'
 });
 
