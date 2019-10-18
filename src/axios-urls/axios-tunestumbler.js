cfb5d6a71ffa98278315197350afb6b249759485
CrispiestHashbrown <r_kieu@outlook.com>
2019-10-18 01:40:57 +0100

    Update axios urls to point to the api url

--- src/axios-urls/axios-tunestumbler.js
+++ src/axios-urls/axios-tunestumbler.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL: 'https://api.tunestumbler.ca'
-    baseURL: 'http://localhost:8080/tunestumbler-wrapper-for-reddit'
+    baseURL: 'https://api.tunestumbler.ca/tunestumbler-wrapper-for-reddit'
+    // baseURL: 'http://localhost:8080/tunestumbler-wrapper-for-reddit'
 });
 
 export default instance;
