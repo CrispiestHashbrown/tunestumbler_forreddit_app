db57156cb0fc527073a47232f6425ceaac03fe2d
CrispiestHashbrown <r_kieu@outlook.com>
2020-01-16 05:39:34 +0000

    Updated Signup container
    - Updated Signup page css styling
    - Implemented React-Bootstrap’s `Form` and `Button`, since it looks
    better and has the same functionality
    - Added Signup form labels in place of placeholders, since it is more
    intuitive
    - Keys have been added to the `Email` and `Password` inputs
    - Disabled default html form validation for email and password

--- src/containers/Signup/Signup.js
+++ src/containers/Signup/Signup.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
+import { Form } from 'react-bootstrap';
 import classes from './Signup.css';
 import * as actions from '../../store/actions/index';
 
@@ -13,11 +14,10 @@ class Signup extends Component {
     state = {
         controls: {
             email: {
-                elementDisplay: 'block',
+                label: 'Email:',
                 elementType: 'input',
                 elementConfig: {
                     type: 'email',
-                    placeholder: 'Email'
                 },
                 value: '',
                 validation: {
@@ -28,11 +28,10 @@ class Signup extends Component {
                 touched: false
             },
             password: {
-                elementDisplay: 'block',
+                label: 'Password:',
                 elementType: 'input',
                 elementConfig: {
                     type: 'password',
-                    placeholder: 'Password'
                 },
                 value: '',
                 validation: {
@@ -92,6 +91,12 @@ class Signup extends Component {
         });
     }
 
+    onEnter = (event) => {
+        if (event.key === "Enter") {
+            this.submitHandler(event);
+        }
+    }
+
     submitHandler = (event) => {
         event.preventDefault();
         this.props.onSignup(this.state.controls.email.value, this.state.controls.password.value);
@@ -107,16 +112,20 @@ class Signup extends Component {
         }
 
         let form = formElementsArray.map(formElement => (
-            <Input 
-                key={formElement.id}
-                elementDisplay={formElement.config.elementDisplay}
-                elementType={formElement.config.elementType}
-                elementConfig={formElement.config.elementConfig}
-                value={formElement.config.value}
-                invalid={!formElement.config.valid}
-                shouldValidate={formElement.config.validation}
-                touched={formElement.config.touched}
-                changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+            <Form.Group key={`group-${formElement.id}`}>
+                <Form.Label className={classes.SignupLabel} >
+                    {formElement.config.label}
+                </Form.Label>
+                <Input 
+                    key={`input-${formElement.id}`}
+                    elementType={formElement.config.elementType}
+                    elementConfig={formElement.config.elementConfig}
+                    value={formElement.config.value}
+                    invalid={!formElement.config.valid}
+                    shouldValidate={formElement.config.validation}
+                    touched={formElement.config.touched}
+                    changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+            </Form.Group>
         ));
 
         if (this.props.loading) {
@@ -146,10 +155,13 @@ class Signup extends Component {
             <div>
                 {signupRedirect}
                 {errorMessage}
-                <form className={classes.Signup} onSubmit={this.submitHandler}>
+                <Form className={classes.Signup} onKeyPress={this.onEnter} noValidate>
                     {form}
-                    <Button buttonType="Successful">Sign up</Button>
-                </form>
+                    <Button 
+                        className={classes.SignupButton} 
+                        buttonType="Successful" 
+                        clicked={this.submitHandler}>Sign up</Button>
+                </Form>
            </div>
         );
     }
